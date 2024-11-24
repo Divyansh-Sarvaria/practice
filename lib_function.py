bce6bb44e83e13d5119a88f0c1c13b466097029a
Divyansh Sarvaria <divyanshsarvaria@gmail.com>
2024-11-25 01:04:32 +0530

    Add files via upload

---
+++ lib_function.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# y = np.array([2005,2019,2023])
+# g = np.array([54.6,72,69,73])
+# plt.plot(y,g)
+# plt.title("graph")
+# plt.xlabel("passing years")
+# plt.ylabel("Student marks")
+# plt.show()
+# trndN = np.array(["python","java","c"])
+# trndP = np.array([20,30,50])
+# plt.pie(trndP)
+# plt.title("popular languages ")
+# plt.legend(trndN)
+# plt.show()
+# 5 years of growth
+yr = np.array([2000,2001,2002,2003,2004,2005])
+pr = np.array([34422,23324,23324,23422,42424,21212])
+plt.bar(yr,pr)
+plt.show()
+plt.draw()
